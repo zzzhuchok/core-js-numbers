57792bd01c082838abbed76e5ccefe5a424e3b37
Rostislav Zhuk <zzzhuchok@gmail.com>
2023-11-14 15:04:41 +0300

    feat: add final part of task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -106,8 +106,13 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const resCos =
+    (x1 * x2 + y1 * y2) /
+    (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2));
+
+  const res = Math.acos(resCos);
+  return res;
 }
 
 /**
